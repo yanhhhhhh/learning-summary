54abe676b07f08db1e363de6edb7e64a9cd2d83d
zhouyanheng <1801084586@qq.com>
2022-04-15 18:05:27 +0800

    canvas、math

---
+++ js/math/random.js
@@ -0,0 +1,8 @@
+let number = Math.random();
+let number10 = Math.round(Math.random() * 10);
+console.log('0-10', number10);
+
+// 随机获取 x~y 之间的一个整数，其中 x 和 y 都是整数。
+// Math.round(Math.random()*(y-x)+x)
+
+console.log('20-100', Math.round(Math.random() * 80 + 20));
