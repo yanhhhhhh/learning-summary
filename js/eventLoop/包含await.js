aad727c9c2a45b074b75df7dc8fc974e9900150d
yanhhhhhh <1801084586@qq.com>
2022-06-07 20:51:47 +0800

    react

--- js/eventLoop/包含await.js
+++ js/eventLoop/包含await.js
@@ -1,3 +1,11 @@
+/* 1. 宏任务：script 脚本、setTimeout、setInterval
+2. 微任务:await promise.then() 
+3.await 是语法糖，之后的代码相当于放在promise.then() 
+
+首先，在执行script脚本的时候，将同步任务推入调用栈中，宏任务和微任务推入任务队列中
+其中，宏任务进入宏任务队列，微任务进入微任务队列
+先清空调用栈之后，清空微任务队列，再清空宏任务队列
+*/
 async function async1() {
   console.log('async1 start')
   await async2()
--- react/react-app/my-app/src/App.js
+++ react/react-app/my-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// demo 01 函数组件
+/* function App() {
+	const click = (params) => {
+		console.log('点击了', params);
+	};
+	return (
+		<div className="App">
+			<button onClick={() => click('传递参数')}>点击了</button>
+		</div>
+	);
+} */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* // demo class组件和setState
+class App extends React.Component {
+	state = {
+		cont: 0,
+		list: [1, 2, 3],
+		person: {
+			name: 'jack',
+			age: 18,
+		},
+	};
+	change = () => {
+		this.setState({
+			cont: this.state.cont + 1,
+			list: [...this.state.list, 4, 5],
+			person: { ...this.state.person, name: 'jae' },
+		});
+	};
+	render() {
+		return (
+			<>
+				<ul>
+					{this.state.list.map((item) => (
+						<li key={item}>{item}</li>
+					))}
+				</ul>
+				<p>{this.state.cont}</p>
+				{/* <p>{this.state.person}</p> */}
+
+				<button onClick={this.change}>change</button>
+			</>
+		);
+	}
 }
-
+ */
 export default App;
