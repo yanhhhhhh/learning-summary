4b9f09bd11f5d97faf3dbc0a7f9a0f08d400d795
ZhouYanHeng <1801084586@qq.com>
2022-06-07 18:15:36 +0800

    new project with recoil

---
+++ react/recoil-example/src/App.js
@@ -0,0 +1,13 @@
+import {
+  RecoilRoot,
+} from 'recoil';
+import CharacterCounter from './components/characterCounter'
+function App() {
+  return (
+   <RecoilRoot>
+     <CharacterCounter/>
+   </RecoilRoot>
+  );
+}
+
+export default App;
