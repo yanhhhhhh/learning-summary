90118aa7e616f97b21f6a8e05ae7456f0d20e01f
yanhhhhhh <1801084586@qq.com>
2023-08-23 14:56:09 +0800

    feat(js):递归flatten数组

--- js/array/flatten.js
+++ js/array/flatten.js
@@ -1,14 +1,31 @@
-function flatten(arr){
-  let reslut =[]
-  for(let i =0;i<arr.length;i++){
-    if(Array.isArray(arr[i])){
-      reslut.push(...flatten(arr[i]))
+//@ts-check
+/**
+ *
+ * @param {Array<any>} array
+ * @return {Array<any>}
+ */
+function flatten(array) {
+  return array.reduce((pre, cur) => {
+    if (Array.isArray(cur)) {
+      pre.push(...flatten(cur));
+    } else {
+      pre.push(cur);
+    }
+    return pre;
+  }, []);
+}
+console.log(flatten([1, [2, 3], [[[[5]]]]]));
 
-    }else{
-      reslut.push(arr[i])
+function flatten(arr) {
+  let reslut = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      reslut.push(...flatten(arr[i]));
+    } else {
+      reslut.push(arr[i]);
     }
   }
-  return reslut
+  return reslut;
 }
-const array =[1,[2,[3,5]]]
-console.log(flatten(array))
+const array = [1, [2, [3, 5]]];
+console.log(flatten(array));
